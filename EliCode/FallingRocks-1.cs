dec296685e256fd3249b0b8a54db9179681135bc
yatashev <yavor.tashev@gmail.com>
2015-09-10 23:24:11 -0700

    Add colors cursor size, increase buffer size

--- EliCode/FallingRocks-1.cs
+++ EliCode/FallingRocks-1.cs
@@ -6,63 +6,64 @@ using System.Threading.Tasks;
 using System.Runtime;
 using System.Threading;
 
+
 class FallingRocks
 {
     static void Main()
     {
-        int width = 100; 
-        int height = 50;  
+        int width = 70;
+        int height = 35;
         Console.SetWindowSize(width, height);
-        Console.SetCursorPosition(0, 0);
-        int ColX = width/2;
+        Console.SetBufferSize(width + 10, height + 10);
+        int ColX = width / 2;
         int RowY = height;
-        
+
 
         Random x = new Random();
         Random y = new Random();
 
-        int x1 = x.Next(0, 100);
-        int y1 = y.Next(0, 10);
+        int x1 = x.Next(0, width);
+        int y1 = y.Next(0, height - 10);
         int y11 = 0;
 
-        int x2 = x.Next(0, 100);
-        int y2 = y.Next(0, 10);
+        int x2 = x.Next(0, width);
+        int y2 = y.Next(0, height - 10);
         int y22 = 0;
 
-        int x3 = x.Next(0, 100);
-        int y3 = y.Next(0, 10);
+        int x3 = x.Next(0, width);
+        int y3 = y.Next(0, height - 10);
         int y33 = 0;
 
-        int x4 = x.Next(0, 100);
-        int y4 = y.Next(0, 10);
+        int x4 = x.Next(0, width);
+        int y4 = y.Next(0, height - 10);
         int y44 = 0;
 
-        int x5 = x.Next(0, 100);
-        int y5 = y.Next(0, 10);
+        int x5 = x.Next(0, width);
+        int y5 = y.Next(0, height - 10);
         int y55 = 0;
 
-        int x6 = x.Next(0, 100);
-        int y6 = y.Next(0, 10);
+        int x6 = x.Next(0, width);
+        int y6 = y.Next(0, height - 10);
         int y66 = 0;
 
-        int x7 = x.Next(0, 100);
-        int y7 = y.Next(0, 10);
+        int x7 = x.Next(0, width);
+        int y7 = y.Next(0, height - 10);
         int y77 = 0;
 
-        int x8 = x.Next(0, 100);
-        int y8 = y.Next(0, 10);
+        int x8 = x.Next(0, width);
+        int y8 = y.Next(0, height - 10);
         int y88 = 0;
 
-        int x9 = x.Next(0, 100);
-        int y9 = y.Next(0, 10);
+        int x9 = x.Next(0, width);
+        int y9 = y.Next(0, height - 10);
         int y99 = 0;
 
-        int x10 = x.Next(0, 100);
-        int y10 = y.Next(0, 10);
+        int x10 = x.Next(0, width);
+        int y10 = y.Next(0, height - 10);
         int y1010 = 0;
 
-        int x11 = x.Next(0, 100);
-        int y112 = y.Next(0, 10);
+        int x11 = x.Next(0, width);
+        int y112 = y.Next(0, height - 10);
         int y1111 = 0;
 
         int j = 0;
@@ -79,6 +80,8 @@ class FallingRocks
 
         for (int i = 0; i < 500; i++)
         {
+            Console.CursorSize = 100;
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.SetCursorPosition(ColX, RowY);
             Console.Write("(0)");
 
@@ -87,78 +90,106 @@ class FallingRocks
                 ConsoleKeyInfo dwarf = Console.ReadKey();
                 switch (dwarf.KeyChar)
                 {
-                    case 'f': Console.SetCursorPosition((ColX + 3), RowY);
+                    case 'f':
+                        Console.SetCursorPosition((ColX + 3), RowY);
+                        Console.CursorSize = 100;
+                        Console.ForegroundColor = ConsoleColor.Magenta;
                         Console.Write("(0)");
+                        if (ColX == 68)
+                        {
+                            Console.SetCursorPosition(width / 2, height);
+                            Console.CursorSize = 100;
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write("(0)");
+                        }
                         break;
-                    case 's': Console.SetCursorPosition((ColX - 3), RowY);
+
+                    case 's':
+                        Console.SetCursorPosition((ColX - 3), RowY);
+                        Console.CursorSize = 100;
+                        Console.ForegroundColor = ConsoleColor.Magenta;
                         Console.Write("(0)");
+                        if (ColX == 5)
+                        {
+                            Console.SetCursorPosition(width / 2, height);
+                            Console.CursorSize = 100;
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write("(0)");
+                        }
                         break;
                 }
 
                 RowY = Console.CursorTop;
-                ColX = Console.CursorLeft-3;
+                ColX = Console.CursorLeft - 3;
             }
             else
             {
                 RowY = Console.CursorTop;
-                ColX = Console.CursorLeft-3;
+                ColX = Console.CursorLeft - 3;
             }
-                                   
-            if (j <= 50-y1 )
+
+            if (j <= height - y1)
             {
-           
-                Console.SetCursorPosition(x1, y1+j);
+                Console.SetCursorPosition(x1, y1 + j);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("^");
                 y11 = y1 + j;
                 j++;
-                
+
             }
             else
             {
-                if (x1 == ColX && y11 == RowY)
+                if (x1 == ColX && y11 == RowY || x1 == ColX + 1 && y11 == RowY || x1 == ColX + 2 && y11 == RowY)
                 {
-                   goto finish;
+                    goto finish;
                 }
-                x1 = x.Next(0, 100);
+                x1 = x.Next(0, width);
                 j = 0;
             }
-            if (k <= 50-y2)
+            if (k <= height - y2)
             {
                 Console.SetCursorPosition(x2, y2 + k);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("@");
                 y22 = y2 + k;
                 k++;
-                
+
             }
             else
             {
-                if (x2 == ColX && y22 == RowY)
+                if (x2 == ColX && y22 == RowY || x2 == ColX + 1 && y22 == RowY || x2 == ColX + 2 && y22 == RowY)
                 {
                     goto finish;
                 }
-                x2 = x.Next(0, 100);
+                x2 = x.Next(0, width);
                 k = 0;
             }
-            if (l <= 50-y3)
+            if (l <= height - y3)
             {
                 Console.SetCursorPosition(x3, y3 + l);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write("*");
                 y33 = y3 + l;
                 l++;
-                
+
             }
             else
             {
-                if (x3 == ColX && y33 == RowY)
+                if (x3 == ColX && y33 == RowY || x3 == ColX + 1 && y33 == RowY || x3 == ColX + 2 && y33 == RowY)
                 {
                     goto finish;
                 }
-                x3 = x.Next(0, 100);
+                x3 = x.Next(0, width);
                 l = 0;
             }
-            if (m <= 50 - y4)
+            if (m <= height - y4)
             {
                 Console.SetCursorPosition(x4, y4 + m);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.Write("&");
                 y44 = y4 + m;
                 m++;
@@ -166,16 +197,18 @@ class FallingRocks
             }
             else
             {
-                if (x4 == ColX && y44 == RowY)
+                if (x4 == ColX && y44 == RowY || x4 == ColX + 1 && y44 == RowY || x4 == ColX + 2 && y44 == RowY)
                 {
                     goto finish;
                 }
-                x4 = x.Next(0, 100);
+                x4 = x.Next(0, width);
                 m = 0;
             }
-            if (n <= 50 - y5)
+            if (n <= height - y5)
             {
                 Console.SetCursorPosition(x5, y5 + n);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.Write("+");
                 y55 = y5 + n;
                 n++;
@@ -183,16 +216,18 @@ class FallingRocks
             }
             else
             {
-                if (x5 == ColX && y55 == RowY)
+                if (x5 == ColX && y55 == RowY || x5 == ColX + 1 && y55 == RowY || x5 == ColX + 2 && y55 == RowY)
                 {
                     goto finish;
                 }
-                x5 = x.Next(0, 100);
+                x5 = x.Next(0, width);
                 n = 0;
             }
-            if (o <= 50 - y6)
+            if (o <= height - y6)
             {
                 Console.SetCursorPosition(x6, y6 + o);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.Write("%");
                 y66 = y6 + o;
                 o++;
@@ -200,16 +235,18 @@ class FallingRocks
             }
             else
             {
-                if (x6 == ColX && y66 == RowY)
+                if (x6 == ColX && y66 == RowY || x6 == ColX + 1 && y66 == RowY || x6 == ColX + 2 && y66 == RowY)
                 {
                     goto finish;
                 }
-                x6 = x.Next(0, 100);
+                x6 = x.Next(0, width);
                 o = 0;
             }
-            if (p <= 50 -y7 )
+            if (p <= height - y7)
             {
                 Console.SetCursorPosition(x7, y7 + p);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write("$");
                 y77 = y7 + p;
                 p++;
@@ -217,16 +254,18 @@ class FallingRocks
             }
             else
             {
-                if (x7 == ColX && y77 == RowY)
+                if (x7 == ColX && y77 == RowY || x7 == ColX + 1 && y77 == RowY || x7 == ColX + 2 && y77 == RowY)
                 {
                     goto finish;
                 }
-                x7 = x.Next(0, 100);
+                x7 = x.Next(0, width);
                 p = 0;
             }
-            if (q <= 50 - y8)
+            if (q <= height - y8)
             {
                 Console.SetCursorPosition(x8, y8 + q);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.Write("#");
                 y88 = y8 + q;
                 q++;
@@ -234,17 +273,19 @@ class FallingRocks
             }
             else
             {
-                if (x8 == ColX && y88 == RowY)
+                if (x8 == ColX && y88 == RowY || x8 == ColX + 1 && y88 == RowY || x8 == ColX + 2 && y88 == RowY)
                 {
                     goto finish;
                 }
-                x8 = x.Next(0, 100);
+                x8 = x.Next(0, width);
                 q = 0;
             }
 
-            if (r <= 50 - y9)
+            if (r <= height - y9)
             {
                 Console.SetCursorPosition(x9, y9 + r);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.Write("!");
                 y99 = y9 + r;
                 r++;
@@ -252,16 +293,18 @@ class FallingRocks
             }
             else
             {
-                if (x9 == ColX && y99 == RowY)
+                if (x9 == ColX && y99 == RowY || x9 == ColX + 1 && y99 == RowY || x9 == ColX + 2 && y99 == RowY)
                 {
                     goto finish;
                 }
-                x9 = x.Next(0, 100);
+                x9 = x.Next(0, width);
                 r = 0;
             }
-            if (s <= 50 - y10)
+            if (s <= height - y10)
             {
                 Console.SetCursorPosition(x10, y10 + s);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write(".");
                 y1010 = y10 + s;
                 s++;
@@ -269,16 +312,18 @@ class FallingRocks
             }
             else
             {
-                if (x10 == ColX && y1010 == RowY)
+                if (x10 == ColX && y1010 == RowY || x10 == ColX + 1 && y1010 == RowY || x10 == ColX + 2 && y1010 == RowY)
                 {
                     goto finish;
                 }
-                x10 = x.Next(0, 100);
+                x10 = x.Next(0, width);
                 s = 0;
             }
-            if (t <= 50 - y112)
+            if (t <= height - y112)
             {
                 Console.SetCursorPosition(x11, y112 + t);
+                Console.CursorSize = 50;
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Console.Write(";");
                 y1111 = y112 + t;
                 t++;
@@ -286,23 +331,28 @@ class FallingRocks
             }
             else
             {
-                if (x11 == ColX && y1111 == RowY)
+                if (x11 == ColX && y1111 == RowY || x11 == ColX + 1 && y1111 == RowY || x11 == ColX + 2 && y1111 == RowY)
                 {
                     goto finish;
                 }
-                x11 = x.Next(0, 100);
+                x11 = x.Next(0, width);
                 t = 0;
             }
 
             Thread.Sleep(150);
             Console.Clear();
-              
+
         }
     finish:
-        Console.WriteLine("You are dead");
+        Console.Clear();
+        Console.SetCursorPosition(width / 2, height / 2);
+        Console.CursorSize = 100;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("YOU ARE DEAD!!!!");
+        Console.ResetColor();
     }
 }
-        
-        
-    
+
+
+
 
